aadaea7aafef521293fda49cda766cdb5d72ba51
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-05-01 01:00:25 -0400

    built out the product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { databaseVersion } = require('../../../UCF-VIRT-FSF-FT-03-2021-U-LOL/13-ORM/01-Activities/03-Ins_Models/config/connection');
 
 const sequelize = require('../config/connection');
 
@@ -6,7 +7,26 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
     sequelize,
