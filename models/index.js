b81d2ecf8ba02ee9d6ba617068349cd8a2c33294
steveSnyder <74982034+stevesnyder@users.noreply.github.com>
2021-05-21 16:13:03 -0400

    finishing touches

--- models/index.js
+++ models/index.js
@@ -8,7 +8,7 @@ const ProductTag = require('./ProductTag');
 Product.belongsTo(Category);
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: product_id
+  foreignKey: "product_id"
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
